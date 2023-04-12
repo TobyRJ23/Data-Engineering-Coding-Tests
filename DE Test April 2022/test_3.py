554e9f5acdf83cf139e73c0d7e5738194eac0837
TobyRJ23 <toby.r.jackson23@gmail.com>
2023-04-12 12:55:32 +0100

    create tests for test 3

--- DE Test April 2022/test_3.py
+++ DE Test April 2022/test_3.py
@@ -2,9 +2,36 @@
 # current time. For example, 01:02:03 should return 6. Improve and fix the function,
 # and write unit test(s) for it. Use any testing framework you're familiar with.
 
+import pytest
+from datetime import datetime
+
 
 # [TODO]: fix the function
 def sum_current_time(time_str: str) -> int:
     """Expects data in the format HH:MM:SS"""
+    if not isinstance(time_str)
+    try:
+        datetime.strptime(time_str, '%H:%M:%S')
     list_of_nums = time_str.split(":")
     return sum(list_of_nums)
+
+
+def test_unexpected_format():
+    """test to see if function returns value error if input unexpected format"""
+    with pytest.raises(ValueError):
+        assert sum_current_time('11.11.11')
+
+def test_unexpected_type():
+    """test to see if function returns type error if input type unexpected"""
+    with pytest.raises(TypeError):
+        assert sum_current_time(459999)
+
+def test_not_valid_time():
+    """test to see if function returns value error if invalid time input"""
+    with pytest.raises(ValueError):
+        assert sum_current_time('45:99:99')
+        
+def test_adds_correctly():
+    """test to see if function returns correct sum of time"""
+    assert sum_current_time('01:02:03') == 6
+    
